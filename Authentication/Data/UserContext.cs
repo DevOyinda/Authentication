fc7799226b32dafdf56aff9ad5ae99019d399255
DevOyinda <kudiratoyindamolad@gmail.com>
2022-11-17 11:43:21 +0100

    Add project files.

---
+++ Authentication/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Authentication.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options): base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity => { entity.HasIndex(e => e.Email).IsUnique(); });
+        }
+    }
+}
